7e29061a3ba7fc4e421caa50619ec8ebc17e9ed5
Sohailhamza <hafizsohail582@gmail.com>
2025-09-15 17:41:11 +0500

    V7 on 15

---
+++ app/src/main/java/com/example/onenew/Splash.java
@@ -0,0 +1,35 @@
+package com.example.onenew;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        ImageView img = findViewById(R.id.splashLogo);
+        Animation zoomIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in);
+        img.startAnimation(zoomIn);
+
+        // 2.5 second ke baad main screen
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(Splash.this, MainActivity.class));
+            finish();
+        }, 2500);
+    }
+}
